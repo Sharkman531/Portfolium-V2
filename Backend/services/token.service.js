f6c93d07ba12719446a4f76d7495e0e8731e97e1
Lautaro Agustin Balsebre <110436512+Sharkman531@users.noreply.github.com>
2024-11-26 21:20:03 -0300

    Update token.service.js

--- Backend/services/token.service.js
+++ Backend/services/token.service.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { MongoClient, ObjectId } from 'mongodb';
 
-const client = new MongoClient("mongodb+srv://admin:admin@dwt4av-hibridas-cluster.z4azp.mongodb.net/")
+const client = new MongoClient(MONGO_URI)
 
 const db = client.db("Portfolium")
 const tokens = db.collection("Tokens")
